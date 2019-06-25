1cef8088fda834e994775aadbb2eb8f9940bcce9
Will Shaughnessy <WillShaughnessy@MacBook-Pro.local>
2019-06-24 21:00:16 -0400

    heroku stuff

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/bill-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-bill-app';
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = process.env.PORT || 8080;
